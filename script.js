9b5c17245c72e3e61b59bd6a39b42d1376b18bf5
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-11 15:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
+	let arr = s.split(" ")
+	return arr[0]
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
 alert(firstWord(s));
